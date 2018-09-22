5ae0b74bf2e80289d83b6439567ffc4b3776d62d
Sandra Alvarez <sandruskiag@gmail.com>
2018-09-23 00:04:49 +0200

    Chase and Escape

---
+++ AI Playground/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public Transform target = null;
+
+    public float speed = 1.0f;
+    public float rotationAngle = 90.0f;
+    public float maxDistanceAway = 5.0f;
+
+    public enum MoveToState { chaseTarget, escapeFromTarget };
+    public MoveToState moveToState = MoveToState.chaseTarget;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private Transform thisTransform = null;
+    #endregion
+
+    #region UNITY_CALLBACKS
+    void Awake()
+    {
+        thisTransform = GetComponent<Transform>();
+	}
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (moveToState == MoveToState.chaseTarget)
+                moveToState = MoveToState.escapeFromTarget;
+            else
+                moveToState = MoveToState.chaseTarget;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Calculate direction
+        Vector3 dir = Vector3.zero;
+
+        switch (moveToState)
+        {
+            case MoveToState.chaseTarget:
+
+                dir = target.position - thisTransform.position;
+                break;
+
+            case MoveToState.escapeFromTarget:
+
+                dir = -(target.position - thisTransform.position);
+                break;
+
+            default:
+                break;
+        }
+
+        dir.y = 0.0f;
+        dir.Normalize();
+
+        // 1. Rotate
+        Quaternion destinationRot = Quaternion.LookRotation(dir, Vector3.up);
+        thisTransform.rotation = Quaternion.RotateTowards(thisTransform.rotation, destinationRot, rotationAngle * Time.deltaTime);
+
+        // 2. Move
+        Vector3 velocity = Vector3.zero;
+
+        switch (moveToState)
+        {
+            case MoveToState.chaseTarget:
+
+                velocity = dir * speed;
+                thisTransform.position += velocity * Time.deltaTime;
+                break;
+
+            case MoveToState.escapeFromTarget:
+
+                if (Vector3.Distance(target.position, thisTransform.position) < maxDistanceAway)
+                {
+                    velocity = dir * speed;
+                    thisTransform.position += velocity * Time.deltaTime;
+                }
+                break;
+
+            default:
+                break;
+        }   
+    }
+    #endregion
+}
---
+++ AI Playground/Assets/Scripts/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public float speed = 1.0f;
+    public float rotationAngle = 90.0f;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private Transform thisTransform = null;
+
+    private float horizontalAxis = 0.0f;
+    private float verticalAxis = 0.0f;
+
+    private Vector3 moveDir = Vector3.zero;
+    private Vector3 rotateDir = Vector3.zero;
+    #endregion
+
+    #region UNITY_CALLBACKS
+    void Awake()
+    {
+        thisTransform = GetComponent<Transform>();
+    } 
+
+    void Update()
+    {
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+        verticalAxis = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Directions
+        moveDir = new Vector3(0.0f, 0.0f, verticalAxis); // local space
+        moveDir = thisTransform.rotation * moveDir; // world space
+        rotateDir = new Vector3(0.0f, horizontalAxis, 0.0f);
+
+        // 1. Rotate
+        thisTransform.rotation *= Quaternion.AngleAxis(rotationAngle * Time.deltaTime, rotateDir);
+
+        // 2. Move
+        Vector3 velocity = moveDir * speed;
+        thisTransform.position += velocity * Time.deltaTime;
+    }
+    #endregion
+}
