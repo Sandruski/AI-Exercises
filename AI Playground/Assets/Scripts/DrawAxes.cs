dd5124a58e392bd226d4938f2c146e0c77952375
Sandra Alvarez <sandruskiag@gmail.com>
2018-09-22 20:54:06 +0200

    Move to target + move away from target

---
+++ AI Playground/Assets/Scripts/DrawAxes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawAxes : MonoBehaviour
+{
+    public float range = 1.0f;
+
+    void OnDrawGizmos()
+    {
+        // Local axes
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * range);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * range);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * range);
+    }
+}
